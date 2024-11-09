d78165cd569b8009f66858c2050b470cce33ab6a
Eventlistener <chanfcsg334@gmail.com>
2024-11-09 23:13:45 +0700

    initialize project

---
+++ src/main/java/com/chanthacode/test/TestController.java
@@ -0,0 +1,12 @@
+package com.chanthacode.test;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+@RestController()
+public class TestController {
+
+    @RequestMapping("/helloworld")
+    public String hello(){
+        return "Hello world";
+    }
+}
